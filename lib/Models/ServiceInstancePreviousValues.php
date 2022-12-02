ba210ecc13c56e75836cd504851c7b0479d2e090
Meetali Solanki <Meetali.Solanki@ibm.com>
2022-12-02 11:11:44 +0550

    Initial Source code

---
+++ lib/Models/ServiceInstancePreviousValues.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * ServiceInstancePreviousValues
+ */
+namespace \Models;
+
+/*
+ * ServiceInstancePreviousValues
+ */
+class ServiceInstancePreviousValues {
+    /* @var string $serviceId  */
+    private $serviceId;
+/* @var string $planId  */
+    private $planId;
+/* @var \\Models\MaintenanceInfo $maintenanceInfo  */
+    private $maintenanceInfo;
+}
---
+++ lib/Models/ServiceInstanceProvisionRequest.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * ServiceInstanceProvisionRequest
+ */
+namespace \Models;
+
+/*
+ * ServiceInstanceProvisionRequest
+ */
+class ServiceInstanceProvisionRequest {
+    /* @var string $serviceId  */
+    private $serviceId;
+/* @var string $planId  */
+    private $planId;
+/* @var \\Models\Context $context  */
+    private $context;
+/* @var object $parameters  */
+    private $parameters;
+/* @var \\Models\MaintenanceInfo $maintenanceInfo  */
+    private $maintenanceInfo;
+}
